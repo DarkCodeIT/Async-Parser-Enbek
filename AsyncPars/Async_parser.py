c5efad057636351615cff7d66b15271d81cd7cbb
DarkCodeIT <maksplay298@gmail.com>
2024-06-01 23:27:15 +0500

    v 1.02

--- AsyncPars/Async_parser.py
+++ AsyncPars/Async_parser.py
@@ -1,126 +1,137 @@
 import json, lxml, time, asyncio, aiohttp, aiofiles
 from icecream import ic
 from bs4 import BeautifulSoup
-from All_data_parser import all_city_data,all_prof_data
-
-#Основной словарь всех нужных данных сайта:
-data = {'Машинист': [], 'Менеджер': [], 'Аналитик': [], 'Дизайнер': [], 'Програмист': [], 'Оператор': [],'Мастер': []
-    , 'Автомеханик': [], 'Администратор': [], 'Косметолог': [], 'Мастер декоративных работ': []
-    , 'Метролог': [], 'Оптик-механик': [], 'Парикмахер': [], 'Строитель': [], 'Разнорабочий': [], 'Повар': [],'Кондитер': []
-    , 'Сантехник': [], 'Сварщик': [], 'Слесарь': [], 'Специалист': [], 'Техник': [],
-    "Учитель" : [], "Тренер" : [], "Санитар" : [], "Врач" : [], "Бухгалтер" : [], "Агроном" : [],
-    "Психолог" : [], "Швея" : [], "Кассир" : [], "Мойщик" : [], "Уборщик" : [], "Дворник" : [],
-    "Инженер" : [], "Рекламный агент" : [], "IT" : [], "Секретарь" : [], "Ведущий специалист" : [],
-    "Юрист" : [],"Адвокат" : []    
-    }
-headers = {"Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-                "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+from const_data import all_city_data,all_prof_data, data, headers
 
 
-
+#variables
+tasks_gather_links_to_vac = []
+tasks_get_data = []
 #Получение информации о вакансии :
-async def get_data(link,prof,city_id):
+async def get_data(url,prof,city_id):
+
     try:
         #Создание сессии подключения :
-        session = aiohttp.ClientSession()
-        async with session.get(url=link,headers=headers) as response:
-            response_text = await response.text()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=link,headers=headers) as response:
+                response_text = await response.text()
 
-            #Начнем парсить все необходимые данные
-            soup = BeautifulSoup(response_text,'lxml')
-            #В переменной data содержится вся нужная инфа
-            data_html = soup.find('div',class_='col-lg-9 order-0')
+                #Начнем парсить все необходимые данные
+                soup = BeautifulSoup(response_text,'lxml')
+                #В переменной data содержится вся нужная инфа
+                data_html = soup.find('div',class_='col-lg-9 order-0')
 
-            #Название профессии
-            name = data_html.find('h4',class_='title')
-            name = name.text
+                #Название профессии
+                name = data_html.find('h4',class_='title')
+                name = name.text
 
-            #ЗП професии
-            price = data_html.find('div',class_='price')
-            price = price.text
+                #ЗП професии
+                price = data_html.find('div',class_='price')
+                price = price.text
 
-            #Тип занятости професии
-            type_work = data_html.find('ul',class_='info d-flex flex-column').find('li').find_all('span')
-            type_work = type_work[1].text
+                #Тип занятости професии
+                type_work = data_html.find('ul',class_='info d-flex flex-column').find('li').find_all('span')
+                type_work = type_work[1].text
 
-            #Обязанности професии
-            rslit = data_html.find_all('div',class_='single-line')[2].find('div',class_='value').text.replace("\n","").strip().split(';')
-            for u in range(len(rslit)):
-                rslit[u] = rslit[u].strip()
+                #Обязанности професии
+                rslit = data_html.find_all('div',class_='single-line')[2].find('div',class_='value').text.replace("\n","").strip().split(';')
 
-            #Дата публикации
-            date = data_html.find('ul',class_='info small mb-2').find('li',class_='mb-0').text.strip()
-            #Парсинг данных окончен
-            #Сохраняем данные
-            prof_dict = {'name' : name,'price' : price,'type_work' : type_work,'responsibilities' : rslit,'date' : date,'link_vac' : link,'prof' : prof,'city_id' : city_id}
-            data[prof].append(prof_dict)
+                for u in range(len(rslit)):
+                    rslit[u] = rslit[u].strip()
+
+                #Дата публикации
+                date = data_html.find('ul',class_='info small mb-2').find('li',class_='mb-0').text.strip()
+
+                #Сохраняем данные
+                prof_dict = {'name' : name,'price' : price,'type_work' : type_work,'responsibilities' : rslit,'date' : date,'link_vac' : link,'prof' : prof,'city_id' : city_id}
+                data[prof].append(prof_dict)
             
     except Exception as ex:
-        print(ex)
-        
-    finally:
-        await session.close()
+        ic(f"Problem in get_data()\n{ex}")
 
 
 #Получение ссылок на страницу вакансии :
-async def gather_data():
-    
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        i=0
-        # Получаем ссылки на профессии во всех городах
-        for city in all_city_data.keys():
-            city_id = all_city_data[city]
-            
-            for prof in all_prof_data:
+async def gather_links_to_vac(url: str, session: aiohttp.ClientSession):
+    ic("Start")
+    try:
+        async with session.get(url=url, headers=headers) as response:
+                            
+                #Создаю объект соупа для парсинга данных
+                soup = BeautifulSoup(await response.text(),'lxml')
+
+                #Ссылки на вакансию содержатся в классе item-list:
+                item_list = soup.find('div', class_="container mb-5").find('div', class_='row').find('div',class_='col-lg-8 col-xxl-9 position-relative content-search-vacancy').find_all('div', class_='item-list')
+
+                for item in item_list:
+                    link = f"https://www.enbek.kz" + item.find('a',class_='stretched').get('href')
+                    tasks_get_data.append(get_data(url=link))
+                    
+                                    
+    except Exception as ex:
+        ic(f"Problem in gather_data()\n{ex}")
+
+    ic("end")
+async def create_main_link(city_id: int, session: aiohttp.ClientSession) -> None:
+
+    for prof in all_prof_data:
+        url = f"https://www.enbek.kz/ru/search/vacancy?prof={prof}&except[subsidized]=subsidized&region_id={city_id}"
+
+        # async with aiohttp.ClientSession() as session:
+        async with session.get(url=url, headers=headers) as response:
+
+            soup = BeautifulSoup(await response.text(), "lxml")
+
+            #Смотрим есть ли пагинация на странице
+            try:
+                max_page_num = soup.find("div", class_="container mb-5").find("div", class_="col-lg-8 col-xxl-9 position-relative content-search-vacancy").find('ul', class_="pagination").find_all("li", class_="page")[-1].text
+
+            except AttributeError as attr:
+                ic(attr)
+
+                #Проверяем есть ли вакансии на странице
+                text = soup.find('div',class_="container mb-5").find('div',class_='row').find('div',class_='col-lg-8 col-xxl-9 position-relative content-search-vacancy').find_all('div', class_="item-list")
+                    
+                if not text:
+                    continue
+                else:
+                    tasks_gather_links_to_vac.append(
+                        gather_links_to_vac(url=url, session=session)
+                    )
+                    continue
                 
-                for page in range(1,50):
+            #Пагинация на странице есть ->
+            try:
+                for i in range(1, int(max_page_num) + 1):
+                    tasks_gather_links_to_vac.append(
+                        gather_links_to_vac(
+                            url=f"https://www.enbek.kz/ru/search/vacancy?prof={prof}&except[subsidized]=subsidized&region_id={city_id}&page={i}",
+                            session=session
+                        )
+                    )
 
-                    url = f"https://www.enbek.kz/ru/search/vacancy?prof={prof}&except[subsidized]=subsidized&region_id={city_id}&page={page}"
-                    #Отправляю запрос на сайт
-                    try:
-                        async with session.get(url=url, headers=headers) as response:
-                        # response = await session.get(url=url, headers=headers)
-                            
-                            #Создаю объект соупа для парсинга данных
-                            soup = BeautifulSoup(await response.text(),'lxml')
-    
-                            #Проверка на наличие на странице вакансий
-                            text = soup.find('div',class_="container mb-5").find('div',class_='row').find('div',class_='col-lg-8 col-xxl-9 position-relative content-search-vacancy').find('div',class_="text-center")
-                            
-                            if str(text) == 'None':
-                                i += 1
-                                ic(i)
-                                    
-                                #Продолжааем парсить данные
-                                #Ссылки на вакансию содержатся в классе item-list:
-                                item_list = soup.find('div', class_="container mb-5").find('div', class_='row').find('div',class_='col-lg-8 col-xxl-9 position-relative content-search-vacancy').find_all('div', class_='item-list')
-
-                                for item in item_list:
-                                    link = f"https://www.enbek.kz" + item.find('a',class_='stretched').get('href')
-                                    task = asyncio.create_task(get_data(link,prof,city_id))
-                                    tasks.append(task)
-                                    
-                            else:
-                                #На странице нету вакансий останавливаем цикл
-                                break
-
-                    except Exception as ex:
-                            print(ex)
-
-                    finally:
-                        pass
-    ic("Запускаем собранные задачи...")
-    await asyncio.gather(*tasks)
+            except Exception as ex:
+                ic(f"Problem in create_main_link()\n{ex}")
+                continue
 
 
 #Главная функция :
-def main():
-    asyncio.run(gather_data())
-    with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False)
+async def main() -> None:
+    async with aiohttp.ClientSession() as session:
+        main_link_task = []
+
+        for key in all_city_data.keys():
+            main_link_task.append(create_main_link(city_id=all_city_data[key], session=session))
+
+        await asyncio.gather(*main_link_task)
+        await asyncio.gather(*tasks_gather_links_to_vac)
+
+
+
+    #создаем дамп данных
+    # async with aiofiles.open("Collected_data.json", 'w', encoding='utf-8') as file:
+    #     json.dump(data, file, ensure_ascii=False)
 
 
 #Точка входа в программу :
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
